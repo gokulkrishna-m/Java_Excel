6bea49226a3942164dc7c5b01a5e1fb0cadd78cc
Gokul Krishna M <55525974+gokulkrishna-m@users.noreply.github.com>
2021-06-06 10:15:07 +0530

    update js
    functionality changed

--- Download_Excel/WebContent/js/sample.js
+++ Download_Excel/WebContent/js/sample.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
-	$(".nav-tabs a").click(function(){
+	/*$(".nav-tabs a").click(function(){
 		$(this).tab('show');
-	  });
+	  });*/
 	//$("#startdate").datepicker();
 	//$("#enddate").datepicker();
+	sessionStorage.clear();
 	$('#reset').click(function(){
 		$('#projectname,#description').val("");
 		$('.error-description,.error-projectname').html('');
+		$("#projectname").focus();
 	});
 	$('#datereset').click(function(){
 		$('#startdate,#enddate').val("");
 		$('.error-startdate,.error-enddate,.error-nodate').html('');
 	});
 	$('#work_data,#excel_data').click(function(){
-		$('#startdate,#enddate').val("");
+		$('#startdate,#enddate,#getdate').val("");
 		$('#projectname,#description').val("");
 		$('.error-description,.error-projectname').html('');
 		$('.error-startdate,.error-enddate,.error-nodate').html('');
@@ -22,22 +24,32 @@ $(document).ready(function(){
 		var res = formValid();
 		console.log('gokul :'+res);
 		if(res == true){
-			console.log('lavanya :');
+			//console.log('lavanya :');
 			var projectName = $('#projectname').val();
 			var desc = $('#description').val();
-			var request = {"projectName":projectName,"desc":desc};
+			var hour = $('#hours option:selected').val();
+			var date = $('#getdate').val();
+			var d = "";
+			if(date != ""){
+				d = date.split("/")[2]+"/"+date.split("/")[1]+"/"+date.split("/")[0];			
+			}
+			console.log('lavanya :'+date);
+			console.log('lavanya1 :'+d);
+			var request = {"projectName":projectName,"desc":desc,"hour":hour,"date":d};
+			console.log('lavanya :'+JSON.stringify(request));
 			$.ajax({
 				type:"post",
 				data:"jsonobj="+JSON.stringify(request),
 				dataType:"JSON",
 				url:"AjaxController1",
 				success:function(successData,status,xhr){
-					console.log("success :"+successData.message);
+					//console.log("success :"+successData.message);
 					//alert(data.message);     
 					if(successData.status == "1"){
 						//$('#input').hide();
 						  $('.success-message').html('').html('<div class="alert alert-success"><strong>Record inserted successfully!</strong></div>');
-						  $('#projectname,#description').val("");
+						  $('#projectname,#description,#getdate').val("");
+						  $(".success-message").fadeOut(1000);
 					}
 					//$('#result').html("Hello "+successData.message+"...!!");
 				},
@@ -112,18 +124,30 @@ $(document).ready(function(){
 			$('.error-enddate').html('');
 			//window.history.forward();
 			//window.open("excelreport.jsp");
+			//window.sessionStorage();
+			$('#startvalue').val($('#startdate').val());
+			$('#endvalue').val($('#enddate').val());
+			sessionStorage.clear();
+			sessionStorage.setItem("startingdate", $('#startdate').val());
+			sessionStorage.setItem("endingdate", $('#enddate').val());
 			$("#exceldata").submit();
+			$('#startdate,#enddate').val("");
 			return true;
 		}
 	});
 	function formValid(){
 		var projectName = $('#projectname').val();
 		var desc = $('#description').val();
-		if(projectName == "" && desc == ""){
+		var hour = $('#hours option:selected').val();
+		//console.log("hour :"+hour);
+		if(projectName == "" && desc == "" && hour == ""){
 			$('.error-projectname').html('').html('<span class="error text-danger">Please enter <strong>project name!</strong></span>');
 			$('.error-description').html('').html('<span class="error text-danger">Please enter <strong>project description!</strong></span>');
+			$('.error-hour').html('').html('<span class="error text-danger">Please select <strong>hours!</strong></span>');
+			$("#projectname").focus();
 			return false;
-		}else if(projectName == "" && desc != ""){
+		}
+		/*else if(projectName == "" && desc != ""){
 			$('.error-description').html('');
 			$('.error-projectname').html('').html('<span class="error text-danger">Please enter <strong>project name!</strong></span>');
 			return false;
@@ -131,9 +155,32 @@ $(document).ready(function(){
 			$('.error-projectname').html('');
 			$('.error-description').html('').html('<span class="error text-danger">Please enter <strong>project description!</strong></span>');
 			return false;
-		}else{
+		}*/
+		else if(projectName == "" || desc == "" || hour == ""){
 			$('.error-description').html('');
 			$('.error-projectname').html('');
+			$('.error-hour').html('');
+			if(projectName == ""){
+				$('.error-projectname').html('').html('<span class="error text-danger">Please enter <strong>project name!</strong></span>');
+				$("#projectname").focus();
+				return false;
+			}
+			if(desc == ""){
+				$('.error-description').html('').html('<span class="error text-danger">Please enter <strong>project description!</strong></span>');
+				$("#description").focus();
+				return false;
+			}
+			if(hour == ""){
+				$('.error-hour').html('').html('<span class="error text-danger">Please select <strong>hours!</strong></span>');
+				$("#hours").focus();
+				return false;
+			}
+		
+		}
+		else{
+			$('.error-description').html('');
+			$('.error-projectname').html('');
+			$('.error-hour').html('');
 			return true;
 		}
 	}
